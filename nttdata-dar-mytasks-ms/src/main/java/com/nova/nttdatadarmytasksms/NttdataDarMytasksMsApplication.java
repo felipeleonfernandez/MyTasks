d855e1c8522b48a6da3cbdfc6ea71b0d3bd7557f
felipeleonfernandez <felipeleonfernandez@gmail.com>
2021-10-28 19:37:00 +0200

    Basic project Spring Boot structure

---
+++ nttdata-dar-mytasks-ms/src/main/java/com/nova/nttdatadarmytasksms/NttdataDarMytasksMsApplication.java
@@ -0,0 +1,13 @@
+package com.nova.nttdatadarmytasksms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttdataDarMytasksMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttdataDarMytasksMsApplication.class, args);
+	}
+
+}
---
+++ nttdata-dar-mytasks-ms/src/test/java/com/nova/nttdatadarmytasksms/NttdataDarMytasksMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nova.nttdatadarmytasksms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdataDarMytasksMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
